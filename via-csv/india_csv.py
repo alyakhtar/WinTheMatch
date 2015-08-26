6b42c5994aafaa66e1e9970cc1deed2e0c2de855
iCHAIT <cgupta319@gmail.com>
2015-08-26 22:51:38 +0530

    structural changes

--- via-csv/india_csv.py
+++ via-csv/india_csv.py
@@ -10,8 +10,6 @@ def team():
     source_code = requests.get(url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, "lxml")
-    fo = open("India_file.txt", "w")
-    # mat = [[u'\n', u'Player', u'\n', u'Span', u'\n', u'Mat', u'\n', u'Inns', u'\n', u'NO', u'\n', u'Runs', u'\n', u'HS', u'\n', u'Ave', u'\n', u'BF', u'\n', u'SR', u'\n', u'100', u'\n', u'50', u'\n', u'0', u'\n', u'4s', u'\n', u'6s', u'\n', u'\n']]
     mat = [[u'Player', u'Span', u'Mat', u'Inns', u'NO', u'Runs', u'HS',
             u'Ave', u'BF', u'SR', u'100', u'50', u'0', u'4s', u'6s']]
     for tr in soup.findAll('tr', {'class': 'data1'}):
@@ -22,10 +20,8 @@ def team():
                 l.append(fifty)
         mat.append(l)
 
-    fo.write(tabulate(mat, tablefmt="fancy_grid").encode("utf8"))
     with open('India_csv.csv', 'wb') as f:
         writer = csv.writer(f)
         writer.writerows(mat)
-    df = pd.DataFrame(mat)
-    df.to_csv("India_pandas.csv")
+
 team()
