28e45ab98a3eafb17ae9e24f1f9e59d42650bae9
aly akhtar <samurai.aly@gmail.com>
2015-08-30 19:30:17 +0530

    PEP8FIED Code :*
    Signed-off-by: aly akhtar <samurai.aly@gmail.com>

--- normal-txt/india_file_stats.py
+++ normal-txt/india_file_stats.py
@@ -5,6 +5,7 @@ import csv
 import sys
 import pandas as pd
 
+
 def team():
     url = 'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;home_or_away=1;home_or_away=2;home_or_away=3;result=1;result=2;result=3;result=5;spanmax1=25+Aug+2015;spanmin1=25+Aug+2011;spanval1=span;team=6;template=results;type=batting'
     print 'Fetching data...please wait\n'
@@ -12,39 +13,44 @@ def team():
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, "lxml")
     global fo
-    fo = open("India_file.txt", "w")
+    fo = open("India_file_test.txt", "w")
     mat = [[u'Player', u'Span', u'Mat', u'Inns', u'NO', u'Runs', u'HS',
             u'Ave', u'BF', u'SR', u'100', u'50', u'0', u'4s', u'6s']]
     m = []
     i = 0
-    j = 0
+    global done
+    done = 0
     for tr in soup.findAll('tr', {'class': 'data1'}):
         l = []
+        if done == 46:
+            i = 100
         for td in tr('td'):
             for a in td('a', {'class': 'data-link'}):
                 abc = a.get('href')
                 name = abc.split('/')
                 # print name[4]
-                player_stats(name[4],i,j)
+                player_stats(name[4], i)
                 # m.append(abc)
             fifty = td.string
             if fifty is not None and fifty != "\n":
                 l.append(fifty)
         i += 2.08
-        j += 1                
+        done += 1
         mat.append(l)
     print '\nSearch Completed!!'
     # print tabulate(mat)
     # fo.write(tabulate(mat,tablefmt="fancy_grid").encode("utf8"))
 
 
-def player_stats(player,i,j):
-    stats = 'http://stats.espncricinfo.com/ci/engine/player/'+player +'?class=2;home_or_away=1;home_or_away=2;home_or_away=3;result=1;result=2;result=3;result=5;spanmax1=25+Aug+2015;spanmin1=25+Aug+2011;spanval1=span;team=6;template=results;type=batting;view=innings'
-    player_name(stats,i,j)
+def player_stats(player, i):
+    stats = 'http://stats.espncricinfo.com/ci/engine/player/'+player + \
+        '?class=2;home_or_away=1;home_or_away=2;home_or_away=3;result=1;result=2;result=3;result=5;spanmax1=25+Aug+2015;spanmin1=25+Aug+2011;spanval1=span;team=6;template=results;type=batting;view=innings'
+    player_name(stats, i)
     source_code = requests.get(stats)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, "lxml")
-    mt = [[u'Runs', u'Mins', u'BF', u'4s', u'6s', u'SR', u'Pos',u'Dismissal', u'Inns',u'Ground', u'Start Date', u'ODI NO.']]
+    mt = [[u'Runs', u'Mins', u'BF', u'4s', u'6s', u'SR', u'Pos',
+           u'Dismissal', u'Inns', u'Ground', u'Start Date', u'ODI NO.']]
     res = [u'Result']
     opp = [u'Opposition']
     for tr in soup.findAll('tr', {'class': 'data1'}):
@@ -54,7 +60,7 @@ def player_stats(player,i,j):
                 str1 = '/ci/content/team/'
                 pqr = a.get('href')
                 if pqr.find(str1) > -1:
-                    pol= a.string
+                    pol = a.string
                     opp.append(pol)
             for a in td('a', {'title': 'view the scorecard for this row'}):
                 xyz = a.get('href')
@@ -70,20 +76,21 @@ def player_stats(player,i,j):
     for x in xrange(len(res)):
         mt[x].append(opp[x])
         mt[x].append(res[x])
-    fo.write(tabulate(mt,tablefmt="fancy_grid").encode("utf8"))
+    fo.write(tabulate(mt, tablefmt="fancy_grid").encode("utf8"))
 
-def player_name(url,i,done):
+
+def player_name(url, i):
     source_code = requests.get(url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, "lxml")
     for link in soup.findAll('h1', {'class': 'SubnavSitesection'}):
         data = link.get_text()
     name = data.split('/')
-    sys.stdout.write("\r[%s%s] %d%% Completed" % ('=' * done, ' ' * (48-done), i) ) 
+    sys.stdout.write("\r[%s%s] %d%% Completed" %
+                     ('=' * done, ' ' * (47-done), i))
     # sys.stdout.write("\r%d%%\n Completed" % i)
     # print str(i)+"% Completed\n"
     fo.write("\n"+name[2]+"\n")
-
 
 
 def match_result(url):
