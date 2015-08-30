44c37b537fc32b44600dca1aecb43372a027e349
iCHAIT <cgupta319@gmail.com>
2015-08-31 01:04:25 +0530

    minor change

--- normal-txt/india_file_stats.py
+++ normal-txt/india_file_stats.py
@@ -1,9 +1,8 @@
 import requests
 from tabulate import tabulate
 from bs4 import BeautifulSoup
-import csv
 import sys
-import pandas as pd
+
 
 def team():
     url = 'http://stats.espncricinfo.com/ci/engine/stats/index.html?class=2;home_or_away=1;home_or_away=2;home_or_away=3;result=1;result=2;result=3;result=5;spanmax1=25+Aug+2015;spanmin1=25+Aug+2011;spanval1=span;team=6;template=results;type=batting'
@@ -15,7 +14,6 @@ def team():
     fo = open("India_file_test.txt", "w")
     mat = [[u'Player', u'Span', u'Mat', u'Inns', u'NO', u'Runs', u'HS',
             u'Ave', u'BF', u'SR', u'100', u'50', u'0', u'4s', u'6s']]
-    m = []
     i = 0
     global done
     done = 0
@@ -28,26 +26,28 @@ def team():
                 abc = a.get('href')
                 name = abc.split('/')
                 # print name[4]
-                player_stats(name[4],i)
+                player_stats(name[4], i)
                 # m.append(abc)
             fifty = td.string
             if fifty is not None and fifty != "\n":
                 l.append(fifty)
         i += 2.08
-        done += 1                
+        done += 1
         mat.append(l)
     print '\n\nSearch Completed!!'
     # print tabulate(mat)
     # fo.write(tabulate(mat,tablefmt="fancy_grid").encode("utf8"))
 
 
-def player_stats(player,i):
-    stats = 'http://stats.espncricinfo.com/ci/engine/player/'+player +'?class=2;home_or_away=1;home_or_away=2;home_or_away=3;result=1;result=2;result=3;result=5;spanmax1=25+Aug+2015;spanmin1=25+Aug+2011;spanval1=span;team=6;template=results;type=batting;view=innings'
-    player_name(stats,i)
+def player_stats(player, i):
+    stats = 'http://stats.espncricinfo.com/ci/engine/player/'+player + \
+        '?class=2;home_or_away=1;home_or_away=2;home_or_away=3;result=1;result=2;result=3;result=5;spanmax1=25+Aug+2015;spanmin1=25+Aug+2011;spanval1=span;team=6;template=results;type=batting;view=innings'
+    player_name(stats, i)
     source_code = requests.get(stats)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, "lxml")
-    mt = [[u'Runs', u'Mins', u'BF', u'4s', u'6s', u'SR', u'Pos',u'Dismissal', u'Inns',u'Ground', u'Start Date', u'ODI NO.']]
+    mt = [[u'Runs', u'Mins', u'BF', u'4s', u'6s', u'SR', u'Pos',
+           u'Dismissal', u'Inns', u'Ground', u'Start Date', u'ODI NO.']]
     res = [u'Result']
     opp = [u'Opposition']
     for tr in soup.findAll('tr', {'class': 'data1'}):
@@ -57,7 +57,7 @@ def player_stats(player,i):
                 str1 = '/ci/content/team/'
                 pqr = a.get('href')
                 if pqr.find(str1) > -1:
-                    pol= a.string
+                    pol = a.string
                     opp.append(pol)
             for a in td('a', {'title': 'view the scorecard for this row'}):
                 xyz = a.get('href')
@@ -73,20 +73,22 @@ def player_stats(player,i):
     for x in xrange(len(res)):
         mt[x].append(opp[x])
         mt[x].append(res[x])
-    fo.write(tabulate(mt,tablefmt="fancy_grid").encode("utf8"))
+    fo.write(tabulate(mt, tablefmt="fancy_grid").encode("utf8"))
 
-def player_name(url,i):
+
+def player_name(url, i):
     source_code = requests.get(url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, "lxml")
     for link in soup.findAll('h1', {'class': 'SubnavSitesection'}):
         data = link.get_text()
     name = data.split('/')
-    sys.stdout.write("\r[%s%s] %d%% Completed" % ('=' * done, ' ' * (47-done), i) ) 
+    sys.stdout.write("\r[%s%s] %d%% Completed" %
+                     ('=' * done, ' ' * (47-done), i))
+    sys.stdout.flush()
     # sys.stdout.write("\r%d%%\n Completed" % i)
     # print str(i)+"% Completed\n"
     fo.write("\n"+name[2]+"\n")
-
 
 
 def match_result(url):
