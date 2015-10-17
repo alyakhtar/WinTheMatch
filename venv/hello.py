4f5c68f72cfe4bfaaea102dd77f675e43e466ab5
iCHAIT <cgupta319@gmail.com>
2015-10-17 16:07:17 +0530

    index page created

--- venv/hello.py
+++ venv/hello.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello(name=None):
-    return render_template('hello.html', name=name)
+def hello():
+    return render_template('hello.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
